ceaa4dbe88dd20e2e993805eb5af6375108933fa
Matty Mahoney <matty@zacksmohawk.com>
2019-12-08 09:17:07 +0000

    Added getFirstLinkFromGridItem method, now tests all pass again.

--- src/main/java/org/sainsburys/PageProcessor.java
+++ src/main/java/org/sainsburys/PageProcessor.java
@@ -2,11 +2,20 @@ package org.sainsburys;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class PageProcessor {
 	public Elements getAllElementsOfType(String content, String elementType) {
 		Document doc = Jsoup.parse(content);
 		return doc.select(elementType);
+	}
+	
+	public Element getFirstLinkFromGridItem(Element gridItem) {
+		Elements links = getAllElementsOfType(gridItem.toString(), "a[href]");
+		if (links.size() > 0) {
+			return links.get(0);
+		}
+		return null;
 	}
 }
