2f4406cdfc9f0f0703b289c5f7f345fbf0eb2402
Matty Mahoney <matty@zacksmohawk.com>
2019-12-08 22:48:08 +0000

    Added test for extracting key attributes from product detail page.
    I skipped the need to prove we can read data from a URL, and put the
    product page into test resources. We are testing the ability to extract
    specific elements from a page in that precise format.

--- src/test/java/org/sainsburys/ScrapeToolTest.java
+++ src/test/java/org/sainsburys/ScrapeToolTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
 
 import org.apache.commons.io.FileUtils;
@@ -74,5 +75,28 @@ public class ScrapeToolTest {
 		
 		String linkURL = pageProcessor.getFirstLinkURLFromGridItem(gridItem);
 		assertEquals(linkURL, "https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/shop/gb/groceries/berries-cherries-currants/sainsburys-british-strawberries-400g.html");
+	}
+	
+	@Test
+	public void when_ProductInfoReadFromLink_then_CanExtractTheFourRequiredAttributes() {
+		String content;
+		try {
+			content = FileUtils.readFileToString(new File("src/test/resources/product-detail-page-source.html"), StandardCharsets.UTF_8);
+		}
+		catch (IOException e) {
+			fail("Could not read source file from test resources");
+			return;
+		}
+		
+		String productContent = pageProcessor.getProductContent(content).toString();
+		assertNotNull(productContent);
+		String title = pageProcessor.getTitle(productContent);
+		assertEquals(title, "Sainsbury's Strawberries 400g");
+		int kcalValue = pageProcessor.getKcalPer100g(productContent);
+		assertEquals(kcalValue, 33);
+		BigDecimal pricePerUnit = pageProcessor.getPricePerUnit(productContent);
+		assertEquals(pricePerUnit, new BigDecimal(1.75));
+		String description = pageProcessor.getDescription(productContent);
+		assertEquals(description, "by Sainsbury's strawberries");
 	}
 }
