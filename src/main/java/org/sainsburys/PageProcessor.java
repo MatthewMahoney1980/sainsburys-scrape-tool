054700998539c7863f069475ba16918987db672c
Matty Mahoney <matty@zacksmohawk.com>
2019-12-09 22:37:21 +0000

    Implemented functionality in PageProcessor to scrape entire page.
    I have also beautified the JSON response for readability, but also so I
    may refactor the tests to use this "ideal" result to compare against in
    a future commit. All tests now pass again.

--- src/main/java/org/sainsburys/PageProcessor.java
+++ src/main/java/org/sainsburys/PageProcessor.java
@@ -1,13 +1,26 @@
 package org.sainsburys;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
 
+import org.apache.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.sainsburys.objects.Result;
+import org.sainsburys.objects.ResultWithKCal;
+import org.sainsburys.objects.ScrapedPage;
+import org.sainsburys.objects.Total;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class PageProcessor {
+	
+	public final static Logger logger = Logger.getLogger(PageProcessor.class);
+	
 	public Elements getAllElementsOfType(String content, String elementType) {
 		Document doc = Jsoup.parse(content);
 		return doc.select(elementType);
@@ -40,8 +53,12 @@ public class PageProcessor {
 		return getFirstElementOfType(content, "h1").text();
 	}
 	
-	public int getKcalPer100g(String content) {
-		String kcalString = getFirstElementOfType(content, "td.nutritionLevel1").text();
+	public Integer getKcalPer100g(String content) {
+		Element kcalElement = getFirstElementOfType(content, "td.nutritionLevel1");
+		if (kcalElement == null) {
+			return null;
+		}
+		String kcalString = kcalElement.text();
 		return Integer.parseInt(kcalString.substring(0, kcalString.indexOf("k")));
 	}
 	
@@ -54,5 +71,74 @@ public class PageProcessor {
 	public String getDescription(String content) {
 		String productText = getFirstElementOfType(content, "div.productText").toString();
 		return getFirstElementOfType(productText, "p").text();
+	}
+	
+	public String scrapePage(String url) {
+		PageReader pageReader = new PageReader();
+		String content = null;
+		try {
+			content = pageReader.readWebPage(url);
+		}
+		catch (Exception e) {
+			logger.error("Could not read data source from url: " + url);
+			System.exit(0);
+		}
+		return getProcessedJSON(content);
+	}
+	
+	public String getProcessedJSON(String content) {
+		BigDecimal gross = new BigDecimal(0);
+		ArrayList<Result> resultsArrayList = new ArrayList<Result>();
+		
+		Elements gridItems = getAllElementsOfType(content, "li.gridItem");
+		for (Element gridItem : gridItems) {
+			String linkURL = getFirstLinkURLFromGridItem(gridItem);
+			
+			Result result = getResultFromItemURL(linkURL);
+			if (result == null) {
+				logger.error("Not adding null result to resultsArrayList");
+				continue;
+			}
+			gross = gross.add(result.getUnitPrice());
+			resultsArrayList.add(result);
+		}
+		
+		BigDecimal vat = gross.multiply(new BigDecimal(0.2)).setScale(2, RoundingMode.CEILING);
+		Total total = new Total(gross, vat);
+		Result[] results = new Result[resultsArrayList.size()];
+		results = resultsArrayList.toArray(results);
+		ScrapedPage scrapedPage = new ScrapedPage(results, total);
+		
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String json = gson.toJson(scrapedPage);
+		
+		return json;
+	}
+	
+	private Result getResultFromItemURL(String url) {
+		PageReader pageReader = new PageReader();
+		String content = null;
+		try {
+			content = pageReader.readWebPage(url);
+		}
+		catch (Exception e) {
+			logger.error("Could not read product info data from url: " + url);
+			return null;
+		}
+		String productContent = getProductContent(content).toString();
+		if (productContent == null) {
+			logger.error("Null product content found at: " + url);
+			return null;
+		}
+		
+		String title = getTitle(productContent);
+		Integer kcals = getKcalPer100g(productContent);
+		BigDecimal unitPrice = getPricePerUnit(productContent);
+		String description = getDescription(productContent);
+		
+		if (kcals == null) {
+			return new Result(title, unitPrice, description);
+		}
+		return new ResultWithKCal(title, kcals, unitPrice, description);
 	}
 }
